67153402b65d21ae2d7bdb01ab1de7ac005d7213
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2024-10-25 14:13:36 +0200

    Minor fix, run locally by default

--- web_app.py
+++ web_app.py
@@ -19,4 +19,5 @@ def return_dataframe():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    # When deploying as a public app add host="0.0.0.0" arg, otherwise remove
+    app.run(debug=True)
